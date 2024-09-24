fbcae38c81fe55e2895727ee450455fc9fa38d89
nom519 <nom95834@gmail.com>
2024-09-24 17:06:10 +0900

    20240924作成中

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,3 +3,15 @@ $(document).ready(function(){
 		$(".sp-nav").slideToggle();
 	});
 });
+
+
+$(document).ready(function(){
+    $('.tab-headers li').click(function(){
+        $('.tab-headers li').removeClass('active');
+        $(this).addClass('active');
+
+        $('.tab-content').removeClass('active');
+        var tab_id = $(this).attr('data-tab');
+        $('#' + tab_id).addClass('active');
+    });
+});
